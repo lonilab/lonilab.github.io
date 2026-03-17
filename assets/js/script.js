11f022b408c8684afbcb9b77d9a85d7d82fb2792
Ting-Yat Wong <yatyat0321@gmail.com>
2026-03-17 11:02:47 +0800

    [fix] update design

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,7 +23,6 @@ toggleButton.addEventListener("click", () => {
 
 document.addEventListener("DOMContentLoaded", function () {
   console.log("script loaded");
-
   const yearEl = document.getElementById("current_year");
   if (yearEl) {
     yearEl.textContent = new Date().getFullYear();
